8d78667561da1a36f54951639ed2311d037a2108
Steve Ngo <stevehngo@hotmail.com>
2022-02-01 11:38:00 -0500

    started

---
+++ index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.status(200).send('My Home Page!!!');
+});
+
+app.listen(PORT, () => {
+  console.log('Server is up');
+});
